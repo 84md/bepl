904ed6ad4eca9449acc4cab6079e2c6b91001a51
Mario Dohmen <61635151+84md@users.noreply.github.com>
2021-11-16 22:08:07 +0100

    Update bepl2.py

--- bepl2.py
+++ bepl2.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from math import ceil, floor
+from math import ceil
+import argparse
 
 @dataclass
 class Board:
@@ -9,7 +10,6 @@ class Board:
     thick: float
 
 
-
 @dataclass
 class Ground:
     length: float
@@ -17,12 +17,11 @@ class Ground:
     direction: bool
 
 
-
 class Bepl():
     def __init__(self, ground, board):
         self.ground = ground
         self.full_board = board
-        self.startval = 0.00 
+        self.startval = 0.00
         self.rows = []
         self.lrow = False
         self.RowCount()
@@ -30,12 +29,12 @@ class Bepl():
         self.RowCount()
         self.Rows()
 
+
     def RowCount(self):
         if self.ground.direction:
             self.rowcount = ceil(self.ground.width / self.full_board.width)
         else:
             self.rowcount = ceil(self.ground.length / self.full_board.width)
-        print("rowcount:", self.rowcount)
 
     def LastRow(self):
         if self.ground.direction:
@@ -54,90 +53,84 @@ class Bepl():
             k = 0.00
             r = []
             length = self.ground.width
-            
         while k < length:
-            
             if self.startval > 0:
-                #print("startval", self.startval)
                 if self.lrow:
                     r.append(Board(length=self.startval,
                                    pos=1,
                                    width=self.lastrow,
                                    thick=self.full_board.thick
                                    ))
-                    
                 else:
                     r.append(Board(length=self.startval,
-                                    pos=1,
-                                    width=self.full_board.width,
-                                    thick=self.full_board.thick
-                                    ))
+                                   pos=1,
+                                   width=self.full_board.width,
+                                   thick=self.full_board.thick
+                                   ))
                 k = k + self.startval
                 self.startval = 0.00
                 continue
             else:
                 if length - k > self.full_board.length:
-                    #print("Addboard")
                     if self.lrow:
                         r.append(Board(pos=0,
                                        length=self.full_board.length,
                                        width=self.lastrow,
                                        thick=self.full_board.thick
-                                        ))
+                                       ))
                         k = k + self.full_board.length
-                    
+                        continue
                     else:
                         r.append(self.full_board)
                         k = k + self.full_board.length
                         continue
-                    
-                        
                 else:
                     rst = length - k
+
                     if self.lrow:
                         r.append(Board(length=rst,
-                                        pos=1,
-                                        width=self.lastrow,
-                                        thick=self.full_board.thick
-                                        ))
-                        
+                                       pos=1,
+                                       width=self.full_board.width,
+                                       thick=self.full_board.thick
+                                       ))
                     else:
-                        if self.lrow:
-                            r.append(Board(length=rst,
-                                            pos=1,
-                                            width=self.full_board.width,
-                                            thick=self.full_board.thick
-                                            ))
-                            
-                        else:
-                            r.append(Board(length=rst,
-                                            pos=1,
-                                            width=self.full_board.width,
-                                            thick=self.full_board.thick
-                                            ))
-                            
+                        r.append(Board(length=rst,
+                                       pos=1,
+                                       width=self.full_board.width,
+                                       thick=self.full_board.thick
+                                       ))
 
-                    self.startval = self.full_board.length -rst
+                    self.startval = self.full_board.length - rst
                     k = k + rst
-                    
                     
             return r
 
     def Rows(self):
-        
         for i in range(self.rowcount):
             if i == self.rowcount-1:
                 self.lrow = True
-            row = self.FillRow()
-            print("row", i)
-            print(row)
+            self.rows.append(self.FillRow())
+
+    def PrinBoards(self):
+        for i in range(len(self.rows)):
+            print("Reihe:", i+1)
+            for j in range(len(self.rows[i])):
+                print("%.3f" % self.rows[i][j].width, end="x")
+                print("%.3f" % self.rows[i][j].length, end=" ")
+            print()
 
 
 def Main():
-    g = Ground(length=6.20, width=2.40, direction=False)
-    b = Board(pos=0, length=2.50, width=0.675, thick=0.015)
-    c = Bepl(g, b)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Ground Size", metavar="n", type=float, nargs=2)
 
+    args = parser.parse_args()
+    print(args)
+
+    g = Ground(length=6.20, width=2.40, direction=False)
+    b = Board(pos=0, length=2.00, width=1.25, thick=0.015)
+    c = Bepl(g, b)
+    c.PrinBoards()
 
 
 
